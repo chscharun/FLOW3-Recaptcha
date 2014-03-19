55854a3ab9c6e9db215e71f1a2b3150f389714a2
Michael Knoll <knoll@punkt.de>
2014-03-19 13:10:44 +0700

    [RFT] Refactored captcha Viewhelper to act as a form field view helper.

--- Classes/TYPO3/Recaptcha/ViewHelpers/Form/CaptchaViewHelper.php
+++ Classes/TYPO3/Recaptcha/ViewHelpers/Form/CaptchaViewHelper.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace TYPO3\Recaptcha\ViewHelpers\Form;
 
 use TYPO3\Flow\Annotations as Flow;
@@ -12,10 +11,40 @@ use TYPO3\Fluid\ViewHelpers\Form\AbstractFormFieldViewHelper;
 class CaptchaViewHelper extends AbstractFormFieldViewHelper {
 
 	/**
-	 * @return string
+	 * Holds the default template to be used for rendering the content of the view helper
+	 *
+	 * @var string
+	 */
+	protected $templateResource = 'resource://TYPO3.Recaptcha/Private/Templates/ViewHelpers/Form/Captcha.html';
+
+
+
+	/**
+	 * Initialize arguments.
+	 *
+	 * @return void
+	 */
+	public function initializeArguments() {
+		$this->registerTagAttribute('template', 'string', 'Resource string for the template to be used for rendering the captcha view helper (optional)', FALSE);
+		$this->registerTagAttribute('publicKey', 'string', 'The public key for the Recaptcha account to be used for this captcha (required!)', TRUE);
+	}
+
+
+
+	/**
+	 * @return string The rendered captcha HTML code
 	 */
 	public function render() {
-		return 'hello world!';
+		if ($this->arguments['template']) {
+			$this->templateResource = $this->arguments['template'];
+		}
+		$view = $this->objectManager->get('\TYPO3\Fluid\View\StandaloneView');
+		/* @var $view \TYPO3\Fluid\View\StandaloneView */
+
+		$view->setTemplatePathAndFilename($this->templateResource);
+		$view->assign('fieldNamePrefix', $this->viewHelperVariableContainer->get('TYPO3\Fluid\ViewHelpers\FormViewHelper', 'fieldNamePrefix'));
+		$view->assign('publicKey', $this->arguments['publicKey']);
+		return $view->render();
 	}
 
 }
