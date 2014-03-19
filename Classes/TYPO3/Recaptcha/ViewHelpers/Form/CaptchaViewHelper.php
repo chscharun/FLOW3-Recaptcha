4fcb4bcda69b46ddbb420d09eebf2f36c6dbb65d
Michael Knoll <knoll@punkt.de>
2014-03-19 03:35:27 +0100

    [RFT] Made the captcha viewhelper extending the abstract form field viewhelper

---
+++ Classes/TYPO3/Recaptcha/ViewHelpers/Form/CaptchaViewHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace TYPO3\Recaptcha\ViewHelpers\Widget;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Fluid\ViewHelpers\Form\AbstractFormFieldViewHelper;
+
+/**
+ * @Flow\Scope("prototype")
+ * @api
+ */
+class CaptchaViewHelper extends AbstractFormFieldViewHelper {
+
+	/**
+	 * @return string
+	 */
+	public function render() {
+		
+	}
+
+}
